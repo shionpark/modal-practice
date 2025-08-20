d8ae5c11d38c0816d69af878f495e3fccc0d6245
Shion <seoy1108@gmail.com>
2025-08-20 23:19:38 +0900

    feat: 모달 열림 상태에 따른 동적 스타일 기능 추가 및 ModalProps 타입 정의
    overlay > section 중첩 구조 제거하여 하위 요소 opacity 문제 해결

--- src/components/modals/ModalOverlay.tsx
+++ src/components/modals/ModalOverlay.tsx
@@ -1,27 +1,50 @@
-import { Button } from '../Button';
 import type { ReactNode } from 'react';
+import clsx from 'clsx';
+import { Button } from '../Button';
+import type { ModalProps } from './types';
 
-interface ModalOverlayProps {
+interface ModalOverlayProps extends ModalProps {
   children: ReactNode;
   showCloseBtn?: boolean;
 }
 
 export function ModalOverlay({
+  isModalOpen,
+  closeModal,
   children,
   showCloseBtn = true,
 }: ModalOverlayProps) {
+  const modalOverlayClass = (isOpen: boolean) =>
+    clsx(
+      'modal-overlay',
+      isOpen
+        ? 'opacity-50 pointer-events-auto'
+        : 'opacity-0 pointer-events-none'
+    );
+
+  const modalSectionClass = (isOpen: boolean) =>
+    clsx(
+      'modal-section',
+      isOpen ? 'scale-100 opacity-100' : 'pointer-events-none opacity-0'
+    );
+
   return (
-    <div className="modal-overlay">
-      <div className="modal-section">
+    <>
+      <div className={modalOverlayClass(isModalOpen)} />
+      <div className={modalSectionClass(isModalOpen)}>
         <div className="modal-content">
           {showCloseBtn && (
-            <Button variant="transparent" className="absolute top-4 right-2">
+            <Button
+              variant="transparent"
+              className="absolute top-4 right-2"
+              onClick={closeModal}
+            >
               X
             </Button>
           )}
           {children}
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/components/modals/types.ts
+++ src/components/modals/types.ts
@@ -2,3 +2,8 @@ export interface Action {
   label: string;
   onClick?: () => void;
 }
+
+export interface ModalProps {
+  isModalOpen: boolean;
+  closeModal: () => void;
+}
