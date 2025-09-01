db76c98e9c28041f1a64a0a42f31c332f22dfb07
Shion <seoy1108@gmail.com>
2025-09-02 01:44:42 +0900

    chore: vitest 설정 파일 및 테스트용 라이브러리 추가
    테스트용 컴포넌트 렌더 및 경로 alias 위한 설정 옵션 추가

---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
+    exclude: [
+      'node_modules',
+      '.pnpm',
+      '**/node_modules/**',
+      '**/__tests__/**',
+      '**/*.spec.ts',
+      '**/*.test-d.ts',
+      'e2e',
+    ],
+  },
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+    },
+  },
+});
